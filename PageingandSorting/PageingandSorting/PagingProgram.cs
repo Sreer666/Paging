a285215ab463502f844b21cb3571c9fcf897d4b6
Sree Ram <sreer666@gmail.com>
2019-03-26 20:55:24 -0400

    Worked on Paging

---
+++ PageingandSorting/PageingandSorting/PagingProgram.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PageingandSorting
+{
+   public class PagingProgram
+    {
+        static void Main(string[] args)
+        {
+            Pagining page = new Pagining();
+            page.pagingmethod();
+        }
+    }
+
+    public class Pagining
+    {
+       public void pagingmethod()
+        {
+            // You are trying to do paging by using skip and take operators
+            //declare the pagesize
+            //declare the pagenumber
+            //check the condtion between the pages meaning How many pages you are trying to do.
+            //and print the statment
+
+            List<Student> students = Student.GetAllStudentInfo;
+            int pagenumber = 0;
+
+            Console.WriteLine("Hi Please Select the PageNumbers between 1---- 4");
+
+            if (int.TryParse(Console.ReadLine(), out pagenumber))
+            {
+                if (pagenumber >= 1 && pagenumber <= 4)
+                {
+                    int pagesize = 3;
+                    IEnumerable<Student> results = students.Skip((pagenumber - 1) * pagesize).Take(pagesize);
+                    foreach (Student s in results)
+                    {
+                        Console.WriteLine(s.Name + "\t" + s.StudentId + "\t" + s.TotalMarks);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please Select the PageNumbers rage between 1---- 4");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Select the PageNumbers rage between 1---- 4");
+            }
+        }
+    }
+}
---
+++ PageingandSorting/PageingandSorting/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PageingandSorting
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string StudentId { get; set; }
+
+        public string TotalMarks { get; set; }
+
+
+        public static List<Student> GetAllStudentInfo =
+            new List<Student>()
+            {
+                new Student()
+                {
+                    Name= "Ramesh1",
+                    StudentId= "1",
+                    TotalMarks= "10"
+                },
+                new Student()
+                {
+                    Name= "Suresh1",
+                    StudentId= "1",
+                    TotalMarks= "10"
+                },
+                new Student()
+                {
+                    Name= "Akbar1",
+                    StudentId= "1",
+                    TotalMarks= "10"
+                },
+                new Student()
+                {
+                    Name= "Ramesh2",
+                    StudentId= "2",
+                    TotalMarks= "20"
+                },
+                new Student()
+                {
+                    Name= "Suresh2",
+                    StudentId= "2",
+                    TotalMarks= "20"
+                },
+                new Student()
+                {
+                    Name= "Akbar2",
+                    StudentId= "2",
+                    TotalMarks= "20"
+                },
+                new Student()
+                {
+                    Name= "Ramesh3",
+                    StudentId= "3",
+                    TotalMarks= "30"
+                },
+                new Student()
+                {
+                    Name= "Suresh3",
+                    StudentId= "3",
+                    TotalMarks= "30"
+                },
+                new Student()
+                {
+                    Name= "Akbar3",
+                    StudentId= "3",
+                    TotalMarks= "30"
+                },
+            };
+
+    }
+}
